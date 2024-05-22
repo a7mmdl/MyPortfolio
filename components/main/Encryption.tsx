c465fbf3c1332a4dbce6d356e406cb3eebe00aac
Ahmed <0423rafi@gmail.com>
2024-05-22 17:26:48 +0400

    Update Encryption.tsx

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+
 import { motion } from "framer-motion";
 import { slideInFromTop } from "@/utils/motion";
 import Image from "next/image";
@@ -12,9 +13,15 @@ const Encryption = () => {
           variants={slideInFromTop}
           className="text-[40px] font-medium text-center text-gray-200"
         >
-          Performance <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500"> &nbsp;</span> Security
+          Performance
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+            {" "}
+            &{" "}
+          </span>
+          Security
         </motion.div>
       </div>
+
       <div className="flex flex-col items-center justify-center translate-y-[-50px] absolute z-[20] w-auto h-auto">
         <div className="flex flex-col items-center group cursor-pointer w-auto h-auto">
           <Image
@@ -32,6 +39,7 @@ const Encryption = () => {
             className=" z-10"
           />
         </div>
+
         <div className="Welcome-box px-[15px] py-[4px] z-[20] brder my-[20px] border-[#7042f88b] opacity-[0.9]">
           <h1 className="Welcome-text text-[12px]">Encryption</h1>
         </div>
@@ -41,13 +49,16 @@ const Encryption = () => {
           Secure your data with end-to-end encryption
         </div>
       </div>
+
       <div className="w-full flex items-start justify-center absolute">
-        <Image
-          src="/encryption.gif"
-          alt="Encryption GIF"
-          width={800}
-          height={450}
-          className="w-full h-auto object-contain object-center"
+        <video
+          loop
+          muted
+          autoPlay
+          playsInline
+          preload="false"
+          className="w-full h-auto"
+          src="/encryption.webm/"
         />
       </div>
     </div>
